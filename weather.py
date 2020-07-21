72d64e12e54aef1e617493ff2ddacfd96746da4b
keshva5 <keshavadk@gmail.com>
2020-07-21 12:44:58 +0530

    First Commit

---
+++ weather.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import os
+import pytz
+import requests
+import math
+API_KEY = '116f30f2fe72f7fc6892c7f30003f5af'
+API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}')
+def query_api(city):
+    try:
+        print(API_URL.format(city, API_KEY))
+        data = requests.get(API_URL.format(city, API_KEY)).json()
+    except Exception as exc:
+        print(exc)
+        data = None
+    return data
